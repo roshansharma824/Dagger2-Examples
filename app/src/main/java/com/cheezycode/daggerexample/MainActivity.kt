4684916baaa7726ae6d8d8d0715389ac72152a75
@roshansharma <roshansharma@stackfusion.io>
2022-12-07 13:09:20 +0530

    field @Inject

--- app/src/main/java/com/cheezycode/daggerexample/MainActivity.kt
+++ app/src/main/java/com/cheezycode/daggerexample/MainActivity.kt
@@ -2,17 +2,22 @@ package com.cheezycode.daggerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var userRegistrationService: UserRegistrationService
 
+    @Inject
+    lateinit var emailService: EmailService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val appComponent = DaggerUserRegistrationComponent.builder().build()
-        val userRegistrationService = appComponent.getUserRegistrationService()
+        appComponent.inject(this)
 
         userRegistrationService.registerUser("cheezycode@gmail.com", "11111" )
     }
--- app/src/main/java/com/cheezycode/daggerexample/UserRegistrationComponent.kt
+++ app/src/main/java/com/cheezycode/daggerexample/UserRegistrationComponent.kt
@@ -1,12 +1,9 @@
 package com.cheezycode.daggerexample
 
-import dagger.BindsInstance
 import dagger.Component
-import dagger.Subcomponent
-import javax.inject.Singleton
 
 @Component
 interface UserRegistrationComponent {
 
-    fun getUserRegistrationService(): UserRegistrationService
+    fun inject(mainActivity: MainActivity)
 }
