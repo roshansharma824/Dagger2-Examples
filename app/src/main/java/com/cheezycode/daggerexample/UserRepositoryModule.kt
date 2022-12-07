440d72b8372cabf51c6eff078d4c651ba382c0cb
@roshansharma <roshansharma@stackfusion.io>
2022-12-07 13:41:20 +0530

    @Binds

--- app/src/main/java/com/cheezycode/daggerexample/UserRepositoryModule.kt
+++ app/src/main/java/com/cheezycode/daggerexample/UserRepositoryModule.kt
@@ -1,18 +1,23 @@
 package com.cheezycode.daggerexample
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule {
+abstract class UserRepositoryModule {
 
 //    @Provides
 //    fun getFirebaseRepository(): UserRepository{
 //        return FirebaseRepository()
 //    }
 
-    @Provides
-    fun getSQLRepository(sqlRepository: SQLRepository): UserRepository{
-        return sqlRepository
-    }
+//    @Provides
+//    fun getSQLRepository(sqlRepository: SQLRepository): UserRepository{
+//        return sqlRepository
+//    }
+
+
+    @Binds
+    abstract fun getSQLRepository(sqlRepository: SQLRepository): UserRepository
 }
